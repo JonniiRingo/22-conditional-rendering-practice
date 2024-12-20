f6ddce4178a5fd6df3d5d52e0a4143eca404e066
Jonnii Ringo <jonathansamuels@Jonathans-MBP-2.lan>
2024-12-20 11:32:55 -0800

    Concept works but have to figure out the logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import Form from "./Form";
 import Login from "./login"
 
-var userIsRegistered = false;
+var userIsNotRegistered = false;
 var isLoggedIn = false
 
 function App() {
   return (
     <div className="container">
-      {!userIsRegistered && <Form />}
-      {userIsRegistered && isLoggedIn? <h1>Hello</h1>: <Login />}
+      {userIsNotRegistered && isLoggedIn? <Form /> : <Login />}
+      {/* {userIsRegistered && isLoggedIn? <h1>Hello</h1>: <Login />} */}
     </div>
   );
 }
