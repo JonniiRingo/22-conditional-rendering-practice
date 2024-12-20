cfc24c9631a6007eb007f8e31cdcead43223225d
Jonnii Ringo <jonathansamuels@Jonathans-MBP-2.lan>
2024-12-20 10:43:41 -0800

    Updated index,jsx to react 18 syntax

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+const root = ReactDOM.createRoot(documet.getElementById("root "));
+
+root.render(<App />); 
 
 //Challenge: Without moving the userIsRegistered variable,
 //1. Show Login as the button text if userIsRegistered is true.
