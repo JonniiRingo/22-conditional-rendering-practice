a05b629cef42d295f875abab7cfbd5f5c6429384
Jonnii Ringo <jonathansamuels@MacBookPro-857.lan>
2024-12-20 16:56:23 -0800

    Ternery state handling with nested ternery state operator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import Form from "./Form";
 import Login from "./login"
 
-var userIsNotRegistered = false;
+var userIsRegistered = false;
 var isLoggedIn = false
 
 function App() {
   return (
     <div className="container">
-      {userIsNotRegistered && isLoggedIn? <Form /> : <Login />}
-      {/* {userIsRegistered && isLoggedIn? <h1>Hello</h1>: <Login />} */}
+      {isLoggedIn? (<h1>Hello</h1>): userIsRegistered? (<Login /> ): (<Form />)}
     </div>
   );
 }
